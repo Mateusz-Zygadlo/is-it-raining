4dc81410d0956034b553ebe69737547fae794375
Mateusz <mateuszzygadlo00@gmail.com>
2021-12-15 00:03:30 +0100

    Create configs and first component

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+const FPS = 30;
+const VIDEO_DURATION_IN_SECONDS = 10;
+const VIDEO_DURATION_IN_FRAMES = VIDEO_DURATION_IN_SECONDS * FPS;
+const VIDEO_WIDTH = 1080;
+const VIDEO_HEIGHT = 1920;
+const VIDEO_ID = 'IsIsRaining';
+
+export const VIDEO_CONFIG = {
+  FPS,
+  VIDEO_DURATION_IN_FRAMES,
+  VIDEO_WIDTH,
+  VIDEO_HEIGHT,
+  VIDEO_ID,
+}
+
+export const APP_CONFIG = {
+  CITY: 'Środa Śląska',
+  PERSONAL_PAGE_URL: '',
+  WEATHER_API_KEY: '',
+}
+
+export const COMPOSITION_CONFIG = {
+  VIDEO: {
+    BACKGROUND_COLOR: '#e3d7b1',
+    OUTRO_BACKGROUND_COLOR: '#57450c',
+  },
+  TEXT: {
+    FONT_SIZE: {
+      SMALL: '26px',
+      MEDIUM: '36px',
+      LARGE: '75px',
+      EXTRA_LARGE: '94px',
+    },
+    COLOR: {
+      REGULAR: '#ffffff',
+      ACCENT: '#F0F8EA'
+    }
+  }
+}
---
+++ src/dictionaries/pl_PL.ts
@@ -0,0 +1,19 @@
+export const pl_PL = {
+  intro: {
+    question: 'Czy pada dzisiaj w Środzie Śląskiej'
+  },
+  common: {
+    celsius: 'C',
+  },
+  weatherStates: {
+    raining: 'Pada deszcz!',
+    snowing: 'Pada śnieg!',
+    thunderstorm: 'Paga i jest burza!',
+    sunny: 'Nie pada',
+  },
+  outro: {
+    question: 'Chcesz zobaczyć więcej projektów wykonanych przeze mnie',
+    github: 'Wejdź na mojego Githuba',
+    note: 'Video dostało wygenerowane automatycznie',
+  }
+}
